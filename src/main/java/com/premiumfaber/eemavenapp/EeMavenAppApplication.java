c821abd3fa77f12e3217f28307f2c34607250c10
Twelvesky18 <marciniak.miko@gmail.com>
2019-12-19 09:59:26 +0100

    Added StepDef

--- src/main/java/com/premiumfaber/eemavenapp/EeMavenAppApplication.java
+++ src/main/java/com/premiumfaber/eemavenapp/EeMavenAppApplication.java
@@ -10,4 +10,43 @@ public class EeMavenAppApplication {
         SpringApplication.run(EeMavenAppApplication.class, args);
     }
 
+    public class Event {
+        int Date;
+        public Event(int date) {
+            date = Date;
+        }
+    }
+
+    public class Lesson {
+        int Date;
+        public Lesson(int date) {
+            date = Date;
+        }
+    }
+
+    public class TeacherRepository {
+        int Date;
+        public TeacherRepository(int date) {
+            date = Date;
+        }
+    }
+
+    public class EventRepository {
+        int Date;
+        public EventRepository(int date) {
+            date = Date;
+        }
+    }
+
+    public class Contract {
+        int Date;
+        public Contract(int date) {
+            date = Date;
+        }
+    }
+
+    public class Person {
+        public Person() {
+        }
+    }
 }
